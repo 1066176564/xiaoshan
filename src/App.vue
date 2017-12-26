5a5eb8c83d1bd528a942521777e530ef22267fce
chongjibo <1066176564@qq.com>
2017-12-26 09:25:44 +0800

    构建项目

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+	<div id="app">
+		<!--rem设置75px-->
+		<div class="footer">
+			<ul class="footernav">
+				<li v-for="(data,i) in arr" @click="atv(i)">
+					<router-link :to="router[i]" :class="{active:change == i}" >
+					<dl>
+						<dt>
+							<mu-icon :value="icon[i]" :size="26"/>
+						</dt>
+						<dd>
+							{{data}}
+						</dd>	
+					</dl>
+					</router-link>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'app',
+		data() {
+			return {
+				arr:["首页","分类","消息","周边","我的"],
+				router:["/home","/classify","/message","/around","/myself"],
+				icon:["home","list","mail_outline","satellite","account_box"],
+				change:0
+			}
+		},
+		methods:{
+			atv(i){
+				this.change=i;
+			}
+		}
+	}
+</script>
+
+<style scoped="scoped">
+	.footer{
+		height: 1.306666rem;
+		width: 100%;
+	}
+	.footernav {
+		height: 1.306666rem;
+		border-top: 1px #dedede solid;
+		background: #fff;
+		width: 100%;
+		display: flex;
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		z-index: 999;
+	}
+	.footernav li{
+		width: 20%;
+		text-align: center;
+		justify-content: space-around;
+	}
+	dl dt{
+		width: 0.746666rem;
+		height: 0.586666rem;
+		margin:0 auto;
+		margin-top: 0.173333rem;
+	}
+	li a{
+		color: #666666;
+		text-decoration: none;
+	}
+	.active{
+		color:#f4647a;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import App from './App.vue'
+//引用muse-ui
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+Vue.use(MuseUI)
+//引用mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+//引用swiper
+require('swiper/dist/css/swiper.css')
+var VueAwesomeSwiper = require('vue-awesome-swiper')
+Vue.use(VueAwesomeSwiper)
+//引用路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+//引用懒加载
+import VueLazyLoad from 'vue-lazyload'
+Vue.use(VueLazyLoad,{
+    error:'图片未找到',
+    loading:'./src/img/loging.gif',
+})
+//路由组件
+import home from './component/home/index.vue';
+import classify from './component/classify/index.vue';
+import message from './component/message/index.vue';
+import myself from './component/myself/index.vue';
+import around from './component/around/index.vue';
+
+var router = new VueRouter({
+	mode: 'history',
+	routes:[
+		{path:"/",redirect:"home"},
+		{path:"/classify",component:classify},
+		{path:"/message",component:message},
+		{path:"/home",component:home},
+		{path:"/myself",component:myself},
+		{path:"/around",component:around}
+	]
+})
+
+new Vue({
+  el: '#app',
+  router:router,
+  render: h => h(App)
+})
