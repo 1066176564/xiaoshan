33de5077948cbe0db4969d4cdc21c0578c909896
dxs980725 <1870571038@qq.com>
2017-12-26 14:54:44 +0800

    home

---
+++ src/component/home/head/head.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+
+  	<div id="head">
+
+	    <span class="left"></span>
+	    <p>小善</p>
+	    <span class="right"></span>
+	    
+  	</div>
+
+  </div>
+</template>
+
+<style scoped="scoped">
+
+</style>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
--- src/component/home/index.vue
+++ src/component/home/index.vue
@@ -1,16 +1,41 @@
 <template>
   <div id="app">
-   
+    <heads></heads>
+    <classification></classification>
+	  <dynamic></dynamic>
+	  <Advertisement></Advertisement>
+	  <integral></integral>
+	  <brand></brand>
+	  <Recommend></Recommend>
   </div>
 </template>
 
 <script>
+
+import head from './head/head.vue'
+import classification from './classification/classification.vue'
+import dynamic from './dynamic/dynamic.vue'
+import Advertisement from './Advertisement/Advertisement.vue'
+import integral from './integral/integral.vue'
+import brand from './brand/brand.vue'
+import Recommend from './Recommend/Recommend.vue'
+
 export default {
   name: 'app',
   data () {
     return {
 
     }
+  },
+  components:{
+  	"heads":head,//头部
+  	// "banner":banner,//轮播
+  	"classification":classification,//十个圆（分类）
+  	"dynamic":dynamic,//公司动态
+  	"Advertisement":Advertisement,//广告
+  	"integral":integral,//积分
+  	"brand":brand,//品牌
+  	"Recommend":Recommend,//推荐
   }
 }
 </script>
