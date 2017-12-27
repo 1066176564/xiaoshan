8517fcc2b42b30bcefb1b3bd48378208000787c5
liyuyudi <1796530502@qq.com>
2017-12-27 10:11:55 +0800

    98674

---
+++ src/component/myself/address/order.vue
@@ -0,0 +1,100 @@
+<template>
+  <div id="app">
+   <header>
+   	<div class="back" @click="back()"></div>
+   	<span class="title">我的订单</span>
+   	<div class="information"></div>
+   </header>
+
+   <div style="width:100%; height:1.253333rem;"></div>
+
+   <main>
+  	
+   </main>
+
+   <footer>
+   	
+   </footer>
+  	
+  	<!-- <div style="width:100%; height:1.44rem;"></div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+
+  methods:{
+    back(){
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style scoped="scoped">
+	
+	#app{
+		width: 100%;
+    background: #f2f2f2;
+		position: absolute;
+		left: 0;
+		top: 0;
+		z-index: 9999;
+		margin-bottom: 0;
+	}
+
+  /* 头部固定 */
+	header{
+		width: 100%;
+		height: 1.253333rem;
+		border-bottom:1px solid #f2f2f2; 
+		position: fixed;
+		left: 0;
+		top: 0;
+		z-index: 99;
+		background:#fff; 
+	}
+
+	.back{
+		width: 0.266667rem;
+		height: 0.466667rem;
+		background:url(../../../img/myself/address/xb1.png);
+		background-size: 6.826667rem 6.826667rem; 
+		background-position: -0.453333rem -0.386667rem;
+		position: fixed;
+		top: 0.4rem;
+		left:0.266667rem;
+		z-index: 999;
+	}
+
+	.information{
+		width: 0.506667rem;
+		height: 0.493333rem;
+		background:url(../../../img/myself/address/xb1.png);
+		background-size: 6.826667rem 6.826667rem; 
+		background-position: -1.133333rem -0.386667rem;
+		position: fixed;
+		top: 0.4rem;
+		right:0.266667rem;
+		z-index: 999;
+	}
+
+	.title{
+		width: 100%;
+		float: left;
+		line-height: 1.253333rem;
+		/*margin-left: 40%;*/
+		font-size: 0.48rem;
+		height: 100%;
+		text-align: center;
+	}
+
+	/* 中间的部分 */
+  
+</style>
